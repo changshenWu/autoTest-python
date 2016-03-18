6362be0b0e350707196558aadd01a979af1eecaf
changshenwu <1106432471@qq.com>
2016-03-18 15:47:34 +0800

    添加Logger工具类

---
+++ src/util/Logger.java
@@ -0,0 +1,44 @@
+package util;
+
+public class Logger {
+	
+	public static void log(String  info) {
+		System.out.println(info);
+	}
+	
+	public static void log(boolean x) {
+		System.out.println(x);
+	}
+	
+	public static void log(char x) {
+		System.out.println(x);
+	}
+	
+	public static void log(char[] x) {
+		System.out.println(x);
+	}
+	
+	public static void log(double x) {
+		System.out.println(x);
+	}
+	
+	public static void log(float x) {
+		System.out.println(x);
+	}
+	
+	public static void log(int x) {
+		System.out.println(x);
+	}
+	
+	public static void log(long x) {
+		System.out.println(x);
+	}
+	
+	public static void log(Object obj) {
+		System.out.println(obj);
+	}
+	
+	public static void log() {
+		System.out.println();
+	}
+}
